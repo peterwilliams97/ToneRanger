4429dfdac08471082f0fa75ace7fa7c22d67150f
Peter Williams <peter.williams@papercut.com>
2018-01-29 14:53:56 +1100

    visit fewer redundant links when scraping

--- paper_spider/paper_spider/spiders/pcbot.py
+++ paper_spider/paper_spider/spiders/pcbot.py
@@ -11,12 +11,18 @@ data_root = 'pc_data'
 
 
 def relative(url):
-    assert url.startswith(web_root), (web_root, url)
-    return url[len(web_root):]
+    if url.startswith(web_root):
+        return 'pc_' + url[len(web_root):]
+    return url
 
 
 def local(url):
     return '@_%s' % relative(url).replace('/', '#')
+
+
+def url_key(url):
+    digits = [c for c in url if c.isdigit()]
+    return url.count('&'), max(len(digits) - 5, 0), url.count('?'), len(url), len(digits), url
 
 
 if False:
@@ -26,7 +32,12 @@ if False:
 
 class PcbotSpider(scrapy.Spider):
     name = 'pcbot'
-    allowed_domains = ['www.papercut.com']
+    allowed_domains = [
+        'www.papercut.com',
+        'blog.papercut.com',
+        'portal.papercut.com',
+        # 'papercut.com',
+    ]
     start_urls = [web_root]
 
     def __init__(self):
@@ -35,7 +46,8 @@ class PcbotSpider(scrapy.Spider):
         print('!!! %s' % getcwd())
         print('!!! %s' % self.data_dir)
         makedirs(self.data_dir, exist_ok=True)
-        self.visited = set()
+        self.visited_url = set()
+        self.visited_body = set()
         self.n_saved = 0
         self.t0 = clock()
 
@@ -47,26 +59,53 @@ class PcbotSpider(scrapy.Spider):
         # scrapy.log('Saved file: %4d: %s' % (self.n_saved, filename))
 
     def parse(self, response):
-        # print('*** %s %r %4d' % (response.url, local(response.url), len(self.visited)))
-        self.visited.add(response.url)
+        # print('*** %s %r %4d' % (response.url, local(response.url), len(self.visited_url)))
+        if response.url in self.visited_url:
+            # print('####1')
+            return
+        self.visited_url.add(response.url)
+
+        hsh = hash(response.body)
+        if hsh in self.visited_body:
+            # print('####2')
+            return
+        self.visited_body.add(hsh)
 
         if not isinstance(response, HtmlResponse):
             return
 
         self._save_response(response)
 
-        print('visited=%d saved=%d t=%.1f %s' % (len(self.visited), self.n_saved,
-            clock() - self.t0, response.url), flush=True)
+        print('visited=%d %d saved=%d t=%.1f %s' % (len(self.visited_url), len(self.visited_body),
+            self.n_saved, clock() - self.t0, response.url), flush=True)
 
         assert not response.url.endswith('.jpg')
         assert not response.url.endswith('.png')
 
-        for i, link in enumerate(response.css('a::attr(href)')):
-            next_page = link.extract()
-            # print('- %5d: %s %s %s' % (i, next_page, next_page is not None, next_page not in self.visited))
-            if (next_page is not None) and (next_page not in self.visited):
-                next_page = response.urljoin(next_page)
-                # print('%3d: %s %d' % (i, next_page, len(self.visited)))
-                # yield response.follow()
-                yield scrapy.Request(next_page, callback=self.parse)
-                # print('  @ %3d: %s %d' % (i, next_page, len(self.visited)))
+        linked_pages = [link.extract() for link in response.css('a::attr(href)')]
+        linked_pages = [page for page in linked_pages if page]
+        linked_pages = [response.urljoin(page) for page in linked_pages]
+        linked_pages = [page for page in linked_pages if page not in self.visited_url]
+        linked_pages = [page for page in linked_pages if not
+                        ('?action=edit' in page or
+                         '?action=diff' in page or
+                         '?action=print' in page)]
+        linked_pages.sort(key=url_key)
+        # print(linked_pages[:3], len(linked_pages))
+        # print('%6d linked pages' % len(linked_pages))
+
+        for i, next_page in enumerate(linked_pages):
+
+            # print('- %5d: %s %s %s' % (i, next_page, next_page is not None, next_page not in self.visited_url))
+            # if next_page is None:
+            #     continue
+            # next_page = response.urljoin(next_page)
+            # if next_page in self.visited_url:
+            #     continue
+
+            # if '?action=edit' in next_page or '?action=diff' or '?action=print' in next_page:
+            #     continue
+            # print('%3d: %s %d' % (i, next_page, len(self.visited_url)))
+            # yield response.follow()
+            yield scrapy.Request(next_page, callback=self.parse)
+            # print('  @ %3d: %s %d' % (i, next_page, len(self.visited_url)))

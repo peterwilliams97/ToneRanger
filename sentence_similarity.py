57456f4cbda54a5360b47064563adb57b11531d5
Peter Williams <peter.williams@papercut.com>
2018-01-31 14:12:11 +1100

    improved sentence_similarity

--- sentence_similarity.py
+++ sentence_similarity.py
@@ -3,6 +3,7 @@
 import codecs
 import glob
 import logging
+import time
 import multiprocessing
 import os
 import re
@@ -12,80 +13,126 @@ import sklearn.manifold
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import spacy
+from spacy.symbols import SPACE, PUNCT, NUM, SYM
+import utils
 
 
+do_got = False
+do_lowercase = False
+do_spacy = True
+
+do_tokenize = False
 do_train = False
 do_plots = False
+
+# Dimensionality of the resulting word vectors.
+# More dimensions, more computationally expensive to train but also more accurate
+# more dimensions = more generalized
+num_features = 300
+# Minimum word count threshold.
+min_word_count = 1
+# Number of threads to run in parallel.
+num_workers = multiprocessing.cpu_count()
+# Context window length.
+context_size = 7
+# Downsample setting for frequent words.
+# 0 - 1e-5 is good for this
+downsampling = 1e-3
+
+# Seed for the RNG, to make the results reproducible.
+seed = 1
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # **Download NLTK tokenizer models (only the first time)**
 # nltk.download("punkt")
 # nltk.download("stopwords")
+if do_got:
+    train_dir = "w2v.trained"
+    sentences_path = os.path.join(train_dir, 'sentences.json')
+    model_name = "thrones2vec"
 
-if do_train:
     book_filenames = sorted(glob.glob("data/*.txt"))
     print("Found books:", book_filenames)
 
     # **Combine the books into one string**
     corpus_raw = ""
     for book_filename in book_filenames:
-        print("Reading '{0}'...".format(book_filename))
+        print("Reading '{0}'... ".format(book_filename), end='')
         with codecs.open(book_filename, "r", "utf-8") as book_file:
             corpus_raw += book_file.read()
         print("Corpus is now {0} characters long".format(len(corpus_raw)))
-        print()
 
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
     raw_sentences = tokenizer.tokenize(corpus_raw)
 
+    utils.save_json(sentences_path, raw_sentences)
+
+else:
+    train_dir = "tone.ranger.trained"
+    sentences_path = 'all.sentences.json'
+    model_name = "papercut"
+
+model_name = '%s.%d.w2v' % (model_name, min_word_count)
+
+if do_lowercase:
+    train_dir = '%s.lower' % train_dir
+if do_spacy:
+    train_dir = '%s.spacy' % train_dir
+    nlp = spacy.load('en')
+words_path = os.path.join(train_dir, 'words.json')
+
+
+if do_tokenize:
+    raw_sentences = utils.load_json(sentences_path)
+    if do_lowercase:
+        raw_sentences = [raw.lower() for raw in raw_sentences]
+
     # convert into a list of words
     # remove unnnecessary,, split into words, no hyphens
     # list of words
-    def sentence_to_wordlist(raw):
-        clean = re.sub("[^a-zA-Z]", " ", raw)
-        words = clean.split()
-        return words
+    if do_spacy:
+        def sentence_to_wordlist(raw):
+            sent = nlp(raw)
+            words = []
+            for tok in sent:
+                word = tok.text
+                if tok.pos in {SPACE, NUM, PUNCT, SYM}:
+                    continue
+                words.append(tok.text)
+            return words
+    else:
+        def sentence_to_wordlist(raw):
+            clean = re.sub("[^a-zA-Z]", " ", raw)
+            words = clean.split()
+            return words
 
     # sentence where each word is tokenized
     sentences = []
-    for raw_sentence in raw_sentences:
-        if len(raw_sentence) > 0:
-            sentences.append(sentence_to_wordlist(raw_sentence))
+    print('%7d sentences' % (len(raw_sentences)))
+    interval = max(len(raw_sentences) // 50, 100)
 
-    print(raw_sentences[5])
-    print(sentence_to_wordlist(raw_sentences[5]))
+    t0 = time.clock()
+    for i, raw in enumerate(raw_sentences):
+        if len(raw) > 0:
+            sentences.append(sentence_to_wordlist(raw))
+        if i % interval == 0 and interval:
+            print('%7d (%5.1f%%) %6.1f sec' % (i, 100.0 * i / len(raw_sentences), time.clock() - t0))
+
+    print('raw sentence 5:', raw_sentences[5])
+    print('word list 5:', sentence_to_wordlist(raw_sentences[5]))
 
     token_count = sum([len(sentence) for sentence in sentences])
     print("The book corpus contains {0:,} tokens".format(token_count))
 
+    utils.save_json(words_path, sentences)
+    # assert False
 
-    # ## Train Word2Vec
 
-    #ONCE we have vectors
-    #step 3 - build model
-    #3 main tasks that vectors help with
-    #DISTANCE, SIMILARITY, RANKING
+if do_train:
 
-    # Dimensionality of the resulting word vectors.
-    # More dimensions, more computationally expensive to train but also more accurate
-    # more dimensions = more generalized
-    num_features = 300
-    # Minimum word count threshold.
-    min_word_count = 3
-
-    # Number of threads to run in parallel.
-    num_workers = multiprocessing.cpu_count()
-
-    # Context window length.
-    context_size = 7
-
-    # Downsample setting for frequent words.
-    # 0 - 1e-5 is good for this
-    downsampling = 1e-3
-
-    # Seed for the RNG, to make the results reproducible.
-    seed = 1
+    sentences = utils.load_json(words_path)
 
     thrones2vec = w2v.Word2Vec(
         sg=1,
@@ -99,17 +146,23 @@ if do_train:
 
     thrones2vec.build_vocab(sentences)
     print("Word2Vec vocabulary length:", len(thrones2vec.wv.vocab))
+    print(type(thrones2vec.wv.vocab))
+    assert 'Kelby' in thrones2vec.wv.vocab
+    for sent in sentences:
+        for word in sent:
+            assert word in thrones2vec.wv.vocab, word
+    # assert False
 
     thrones2vec.train(sentences,
                       total_examples=thrones2vec.corpus_count,
                       epochs=thrones2vec.iter)
 
     # **Save to file, can be useful later**
-    os.makedirs("w2v.trained", exist_ok=True)
-    thrones2vec.save(os.path.join("w2v.trained", "thrones2vec.w2v"))
+    os.makedirs(train_dir, exist_ok=True)
+    thrones2vec.save(os.path.join(train_dir, model_name))
 
 # ## Explore the trained model.
-thrones2vec = w2v.Word2Vec.load(os.path.join("w2v.trained", "thrones2vec.w2v"))
+thrones2vec = w2v.Word2Vec.load(os.path.join(train_dir, model_name))
 
 
 if do_plots:
@@ -163,15 +216,43 @@ if do_plots:
     # **Food products are grouped nicely as well. Aerys (The Mad King) being close to "roasted" also looks sadly correct**
     plot_region(x_bounds=(0, 1), y_bounds=(3, 4.5))
 
+
 # ### Explore semantic similarities between book characters
 # **Words closest to the given word**
-thrones2vec.most_similar("Stark")
-thrones2vec.most_similar("Aerys")
-thrones2vec.most_similar("direwolf")
+def show_similer(term):
+    if do_lowercase:
+        term = term.lower()
+    try:
+        similar = thrones2vec.most_similar(term)
+    except KeyError as e:
+        try:
+            similar = thrones2vec.most_similar(term.lower())
+        except KeyError as e:
+            print(e)
+            return
 
+    print('Most similar to %r' % term)
+    for i, (t, v) in enumerate(similar):
+        print('%4d: %15s %.3f' % (i, t, v))
+
+
+if do_got:
+    show_similer("Stark")
+    show_similer("Aerys")
+    show_similer("direwolf")
+else:
+    for term in ["PaperCut", "copier", "Dance", "print", "printer", "printing", "user", "server",
+                 'support', 'NG', 'MF', 'Mobility', 'software', 'company', 'version',
+                 'Windows', 'DNS', 'network', 'PostScript', 'Xerox', 'Konica', 'Ricoh',
+                 'archiving', 'watermark', 'cloud', 'LDAP',
+                 'Peter', 'Kelby', 'Damien', 'Jason', 'Thom', 'Alec', 'Travis', 'Geoff', 'Chris', 'Matt',
+                 'Williams', 'White', 'Clark', 'Beresford', 'Doran', 'Clews', 'Smith', 'Dance']:
+        show_similer(term)
 
 # **Linear relationships between word pairs**
 def nearest_similarity_cosmul(start1, end1, end2):
+    if do_lowercase:
+        start1, end1, end2 = (w.lower() for w in (start1, end1, end2))
     similarities = thrones2vec.most_similar_cosmul(
         positive=[end2, start1],
         negative=[end1]
@@ -181,6 +262,6 @@ def nearest_similarity_cosmul(start1, end1, end2):
     return start2
 
 
-nearest_similarity_cosmul("Stark", "Winterfell", "Riverrun")
-nearest_similarity_cosmul("Jaime", "sword", "wine")
-nearest_similarity_cosmul("Arya", "Nymeria", "dragons")
+nearest_similarity_cosmul("Geoff", "Chris", "paper")
+nearest_similarity_cosmul("copier", "MFP", "save")
+nearest_similarity_cosmul("print", "scan", "Melbourne")

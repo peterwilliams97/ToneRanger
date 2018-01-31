2546f3b0ff7c3ba1b37f9cd99ab2426afe484411
Peter Williams <peter.williams@papercut.com>
2018-01-31 14:40:42 +1100

    improved sentence_similarity

--- sentence_similarity.py
+++ sentence_similarity.py
@@ -29,7 +29,7 @@ do_plots = False
 # Dimensionality of the resulting word vectors.
 # More dimensions, more computationally expensive to train but also more accurate
 # more dimensions = more generalized
-num_features = 300
+num_features = 600
 # Minimum word count threshold.
 min_word_count = 1
 # Number of threads to run in parallel.
@@ -45,6 +45,7 @@ seed = 1
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+
 # **Download NLTK tokenizer models (only the first time)**
 # nltk.download("punkt")
 # nltk.download("stopwords")
@@ -54,15 +55,15 @@ if do_got:
     model_name = "thrones2vec"
 
     book_filenames = sorted(glob.glob("data/*.txt"))
-    print("Found books:", book_filenames)
+    my_print("Found books:", book_filenames)
 
     # **Combine the books into one string**
     corpus_raw = ""
     for book_filename in book_filenames:
-        print("Reading '{0}'... ".format(book_filename), end='')
+        my_print("Reading '{0}'... ".format(book_filename), end='')
         with codecs.open(book_filename, "r", "utf-8") as book_file:
             corpus_raw += book_file.read()
-        print("Corpus is now {0} characters long".format(len(corpus_raw)))
+        my_print("Corpus is now {0} characters long".format(len(corpus_raw)))
 
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
     raw_sentences = tokenizer.tokenize(corpus_raw)
@@ -74,7 +75,8 @@ else:
     sentences_path = 'all.sentences.json'
     model_name = "papercut"
 
-model_name = '%s.%d.w2v' % (model_name, min_word_count)
+model_name = '%s.w%d.f%d.w2v' % (model_name, min_word_count, num_features)
+log_name = '%s.w%d.f%d.log' % (model_name, min_word_count, num_features)
 
 if do_lowercase:
     train_dir = '%s.lower' % train_dir
@@ -82,6 +84,14 @@ if do_spacy:
     train_dir = '%s.spacy' % train_dir
     nlp = spacy.load('en')
 words_path = os.path.join(train_dir, 'words.json')
+log_path = os.path.join(train_dir, log_name)
+
+log_f = open(log_path, 'wt')
+
+
+def my_print(*args):
+    print(args)
+    print(args, file=log_f)
 
 
 if do_tokenize:
@@ -110,7 +120,7 @@ if do_tokenize:
 
     # sentence where each word is tokenized
     sentences = []
-    print('%7d sentences' % (len(raw_sentences)))
+    my_print('%7d sentences' % (len(raw_sentences)))
     interval = max(len(raw_sentences) // 50, 100)
 
     t0 = time.clock()
@@ -118,13 +128,13 @@ if do_tokenize:
         if len(raw) > 0:
             sentences.append(sentence_to_wordlist(raw))
         if i % interval == 0 and interval:
-            print('%7d (%5.1f%%) %6.1f sec' % (i, 100.0 * i / len(raw_sentences), time.clock() - t0))
+            my_print('%7d (%5.1f%%) %6.1f sec' % (i, 100.0 * i / len(raw_sentences), time.clock() - t0))
 
-    print('raw sentence 5:', raw_sentences[5])
-    print('word list 5:', sentence_to_wordlist(raw_sentences[5]))
+    my_print('raw sentence 5:', raw_sentences[5])
+    my_print('word list 5:', sentence_to_wordlist(raw_sentences[5]))
 
     token_count = sum([len(sentence) for sentence in sentences])
-    print("The book corpus contains {0:,} tokens".format(token_count))
+    my_print("The book corpus contains {0:,} tokens".format(token_count))
 
     utils.save_json(words_path, sentences)
     # assert False
@@ -145,8 +155,8 @@ if do_train:
         )
 
     thrones2vec.build_vocab(sentences)
-    print("Word2Vec vocabulary length:", len(thrones2vec.wv.vocab))
-    print(type(thrones2vec.wv.vocab))
+    my_print("Word2Vec vocabulary length:", len(thrones2vec.wv.vocab))
+    my_print(type(thrones2vec.wv.vocab))
     assert 'Kelby' in thrones2vec.wv.vocab
     for sent in sentences:
         for word in sent:
@@ -171,7 +181,7 @@ if do_plots:
 
     all_word_vectors_matrix = thrones2vec.wv.syn0
 
-    print('t-sne')
+    my_print('t-sne')
     all_word_vectors_matrix_2d = tsne.fit_transform(all_word_vectors_matrix)
 
 
@@ -187,11 +197,11 @@ if do_plots:
         columns=["word", "x", "y"]
     )
 
-    print(points.head(10))
-    print(points.describe())
+    my_print(points.head(10))
+    my_print(points.describe())
 
     sns.set_context("poster")
-    print('plot')
+    my_print('plot')
     points.plot.scatter("x", "y", s=10, figsize=(20, 12))
     plt.show()
 
@@ -202,8 +212,8 @@ if do_plots:
             (x_bounds[0] <= points.x) & (points.x <= x_bounds[1]) &
             (y_bounds[0] <= points.y) & (points.y <= y_bounds[1])
         ]
-        print(type(slice))
-        print(slice.describe())
+        my_print(type(slice))
+        my_print(slice.describe())
         ax = slice.plot.scatter("x", "y", s=35, figsize=(10, 8))
         for i, point in slice.iterrows():
             ax.text(point.x + 0.005, point.y + 0.005, point.word, fontsize=11)
@@ -228,12 +238,12 @@ def show_similer(term):
         try:
             similar = thrones2vec.most_similar(term.lower())
         except KeyError as e:
-            print(e)
+            my_print(e)
             return
 
-    print('Most similar to %r' % term)
+    my_print('Most similar to %r' % term)
     for i, (t, v) in enumerate(similar):
-        print('%4d: %15s %.3f' % (i, t, v))
+        my_print('%4d: %15s %.3f' % (i, t, v))
 
 
 if do_got:
@@ -246,7 +256,12 @@ else:
                  'Windows', 'DNS', 'network', 'PostScript', 'Xerox', 'Konica', 'Ricoh',
                  'archiving', 'watermark', 'cloud', 'LDAP',
                  'Peter', 'Kelby', 'Damien', 'Jason', 'Thom', 'Alec', 'Travis', 'Geoff', 'Chris', 'Matt',
-                 'Williams', 'White', 'Clark', 'Beresford', 'Doran', 'Clews', 'Smith', 'Dance']:
+                 'Julie', 'Dean', 'Maria', 'Hendrik', 'Tom', 'Tim',
+                 'Troubleshoot', 'License', 'email', 'Administrator', 'password', 'account',
+                 'Williams', 'White', 'Clark', 'Beresford', 'Doran', 'Clews', 'Smith', 'Dance',
+                 'PaperCutty', 'PaperCutter', 'good', 'bad', 'clever', 'Agile', 'buzzword',
+                 'scrum', 'kanban'
+                 ]:
         show_similer(term)
 
 # **Linear relationships between word pairs**
@@ -258,10 +273,13 @@ def nearest_similarity_cosmul(start1, end1, end2):
         negative=[end1]
     )
     start2 = similarities[0][0]
-    print("{start1} is related to {end1}, as {start2} is related to {end2}".format(**locals()))
+    my_print("{start1} is related to {end1}, as {start2} is related to {end2}".format(**locals()))
     return start2
 
 
 nearest_similarity_cosmul("Geoff", "Chris", "paper")
+nearest_similarity_cosmul("Chris", "Matt", "Peter")
 nearest_similarity_cosmul("copier", "MFP", "save")
-nearest_similarity_cosmul("print", "scan", "Melbourne")
+nearest_similarity_cosmul("print", "scan", "Peter")
+nearest_similarity_cosmul("printer", "copier", "print")
+nearest_similarity_cosmul("printer", "copier", "watermark")

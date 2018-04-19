3bbd719668e15accffad874afdf269566c677cd2
Peter Williams <peter.williams@papercut.com>
2018-04-20 08:52:54 +1000

    Predicting more stuff

--- predict.py
+++ predict.py
@@ -1,5 +1,30 @@
 import spacy
 from utils import load_jsonl
+import numpy as np
+import annoy
+# import faiss
+from annoy import AnnoyIndex
+import random
+
+if False:
+    f = 7
+    t = AnnoyIndex(f)  # Length of item vector that will be indexed
+    for i in range(1000):
+        v = [random.gauss(0, 1) for z in range(f)]
+        t.add_item(i, v)
+
+    t.build(10)  # 10 trees
+    t.save('test.ann')
+
+    # ...
+
+    u = AnnoyIndex(f)
+    u.load('test.ann')  # super fast, will just mmap the file
+    print(u.get_nns_by_item(0, 1000))  # will find the 1000 nearest neighbors
+
+    assert False
+
+
 nlp = spacy.load('predict/my_model')
 
 texts = [
@@ -25,8 +50,53 @@ for i, p in enumerate(paras):
     url = p['meta']['url']
     doc = nlp(text)
     score = doc.cats
+    # print(sorted(score))
+    # assert False
+
     results.append((text, url, score))
-    if i % 100 == 50:
+    if i % 1000 == 50:
         # text = text.decode('utf-8', errors='ignore')
-        text = text.encode('cp850','replace').decode('cp850')
+        text = text.encode('cp850', 'replace').decode('cp850')
         print('%4d: %-80s %s' % (i, text[:80], score))
+
+f = 5
+ASPECTS = ['BRIGHT', 'CARING', 'CONVERSATIONAL', 'EMOTIVE', 'PROFESSIONAL']
+N = len(results)
+
+for aspect in ASPECTS:
+    print('~' * 80)
+    print(aspect)
+    results.sort(key=lambda x: x[2][aspect])
+    for i, (text, url, score) in enumerate(results[:3]):
+        print('%d: %.4f %s' % (i, score[aspect], url))
+    print('...')
+    for n in N // 4, N // 2, 3 * N // 4:
+        for i, (text, url, score) in enumerate(results[n - 1: n + 2]):
+            print('%d: %.4f %s' % (i, score[aspect], url))
+        print('...')
+    for i, (text, url, score) in enumerate(results[-3:]):
+        print('%d: %.4f %s' % (i, score[aspect], url))
+
+t = AnnoyIndex(f)  # Length of item vector that will be indexed
+for i, (text, url, score) in enumerate(results):
+    v = [score[k] for k in ASPECTS]
+    t.add_item(i, v)
+
+t.build(10)  # 10 trees
+t.save('test.ann')
+
+# ...
+
+u = AnnoyIndex(f)
+u.load('test.ann')  # super fast, will just mmap the file
+for i in range(10):
+    text, url, score = results[i]
+    print('^' * 80)
+    print('Docs near %d: %s' % (i, url))
+    nn = u.get_nns_by_item(i, 4)
+    for j, idx in enumerate(nn[1:]):
+        text2, url2, score2 = results[idx]
+        print('%2d: %4d: %s' % (j, idx, url2))
+
+# print(u.get_nns_by_item(0, 10))  # will find the 100 nearest neighbors
+
